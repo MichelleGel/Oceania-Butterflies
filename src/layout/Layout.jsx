dae5b0ccb609229a57fb5b60388ec64ac9816dc1
juliazmor <juliezarco21@gmail.com>
2025-07-16 15:25:34 +0200

    corrected butterfly card without buttons

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,17 +2,14 @@ import { Outlet } from "react-router-dom"
 import NavBar from "../components/NavBar"
 import Footer from "../components/Footer"
 import ButterflyCard from "../components/ButterflyCard"
+import butterfliesData from '../../server/butterfly.json';
 
-const Layout=()=> {
+const Layout = () => {
     return (
         <>
-        <NavBar/>        
-        <hr></hr>
-        <ButterflyCard></ButterflyCard>
-        <hr></hr>
-        <Outlet/>
-        
-        <Footer/>
+            <NavBar />
+            <Outlet />
+            <Footer />
         </>
     )
 }
