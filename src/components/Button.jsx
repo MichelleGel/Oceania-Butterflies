063ad95c59622fad6b1bb7ad66a85f1b5a70506d
juliazmor <juliezarco21@gmail.com>
2025-07-18 00:46:51 +0200

    button was created

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,17 @@
-let Button=()=>{
+import "./Button.css";
+
+let Button = ({ title, onClick, tooltip }) => {
+
+    const handleClick = () => {
+        //console.log("test" + title);
+        if (onClick) {
+            onClick();
+        }
+    };
+
     return (
         <>
-        
+            <button title={tooltip} type="button" onClick={handleClick}>{title}</button>
         </>
     )
 }
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,22 +2,13 @@ import { Outlet } from "react-router-dom"
 import NavBar from "../components/NavBar"
 import Footer from "../components/Footer"
 import ButterflyCard from "../components/ButterflyCard"
-import butterfliesData from '../../server/butterfly.json';
+import butterfliesData from '../../server/butterfly.json'
 
 const Layout = () => {
     return (
         <>
             <NavBar />
             <Outlet />
-            
-            <div className="gallery-container">
-                {
-                    butterfliesData.butterfly.map(butterfly => (
-                        <ButterflyCard key={butterfly.id} butterfly={butterfly} />
-                    ))
-                }
-            </div>
-
             <Footer />
         </>
     )
