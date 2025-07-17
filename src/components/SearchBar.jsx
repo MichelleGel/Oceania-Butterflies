ca806cf0b6de5e29965d371981d202b9422339d6
Camila Arenas <mcarenasdh@gmail.com>
2025-07-17 14:13:51 +0200

    Add clean filter

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,20 +1,24 @@
+import "./searchBar.css"
 //Vamos a crear el componente SearchBar que permitirá buscar mariposas y filtrar dependiendo las opciones que queramos.
-const SearchBar = ({ onSearchChange, onRegionChange }) => { 
-    //Recibirá por ahora dos props: 
-    //onSearchChange se usará cuando alguien escriba en la barra de búsqueda.
-   // onRegionChange se usará cuando alguien haga clic en un botón de región.
+const SearchBar = ({ onSearchChange, onRegionChange, onThreatChange,onClearAll  }) => {
+  //Recibirá por ahora dos props:
+  //onSearchChange se usará cuando alguien escriba en la barra de búsqueda.
+  // onRegionChange se usará cuando alguien haga clic en un botón de región.
   return (
-    <div>
+    <div className="searchContainer">
       <input //Barra de búsqueda normal
-        type="text" 
+        type="text"
         placeholder="Buscar mariposas..."
         onChange={(e) => onSearchChange(e.target.value)} // onChange: Va detectar el cambio, se activa automáticamente (es una propiedad del input). e.target.value = recoge el valor de lo que se está escribiendo en el input.
       />
-      <button onClick={() => onRegionChange('Todas')}>Todas</button>
-      <button onClick={() => onRegionChange('Australia')}>Australia</button>
-      <button onClick={() => onRegionChange('Nueva Zelanda')}>Nueva Zelanda</button>
-      <button onClick={() => onRegionChange('Islas del Pacífico')}>Islas del Pacífico</button>
-    </div>
+      <button onClick={onClearAll}>Todas</button>
+      <button onClick={() => onRegionChange("Australia")}>Australia</button>
+      <button onClick={() => onRegionChange("Nueva Zelanda")}>Nueva Zelanda</button>
+      <button onClick={() => onRegionChange("Islas del Pacífico")}>Islas del Pacífico</button>
+      <button onClick={() => onThreatChange("Vulnerable")}>Vulnerable</button>
+      <button onClick={() => onThreatChange("Preocupación menor")}>Preocupación menor</button>
+      <button onClick={() => onThreatChange("En peligro crítico")}>En peligro crítico</button>
+      </div>
   );
 };
 
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,41 +1,60 @@
-import { useState } from 'react';
-import ButterflyCard from "../components/ButterflyCard"; //Importamos el componente ButterflyCard y que contiene las tarjetas. 
+import { useState } from "react";
+import ButterflyCard from "../components/ButterflyCard"; //Importamos el componente ButterflyCard y que contiene las tarjetas.
 import "./list.css"; //Importamos el CSS que le da estilo a esta página en particular
-import ButterflyData from '/server/butterfly.json' //Importamos nuestra base de datos. 
+import ButterflyData from "/server/butterfly.json"; //Importamos nuestra base de datos.
 import SearchBar from "../components/SearchBar"; //Importamos el componente de SearchBar
 
-const List=()=> {
-  const [searchTerm, setSearchTerm] = useState(''); //Con el useState vamos a guardar el texto de busqueda, lo inicializamos vacio. 
-  const [selectedRegion, setSelectedRegion] = useState('Todas'); //Este es para la Region lo iniciamos en Todas para que se vean todas desde el inicio.
+const List = () => {
+  const [searchTerm, setSearchTerm] = useState(""); //Con el useState vamos a guardar el texto de busqueda, lo inicializamos vacio.
+  const [selectedRegion, setSelectedRegion] = useState("Todas"); //Este es para la Region lo iniciamos en Todas para que se vean todas desde el inicio.
+  const [selectedThreat, setSelectedThreat] = useState("Todas");
+
+    const handleClearFilters = () => {
+    setSearchTerm("");
+    setSelectedRegion("Todas");
+    setSelectedThreat("Todas");
+  };
   // Lógica de filtrado.
   //filteredButterflies va a guardar la lista de las mariposas dependiendo la filtración.
-  const filteredButterflies = ButterflyData.butterfly.filter(butterfly => { //filter() Es una función de JavaScript que crea una nueva lista solo con los elementos que cumplen una condición.
+  const filteredButterflies = ButterflyData.butterfly.filter((butterfly) => {
+    //filter() Es una función de JavaScript que crea una nueva lista solo con los elementos que cumplen una condición.
     // Filtro por región
-    const regionMatch = selectedRegion === 'Todas' || butterfly.region === selectedRegion;
-    
-    // Filtro por texto de búsqueda (insensible a mayúsculas)
-    const searchMatch = butterfly.commonName.toLowerCase().includes(searchTerm.toLowerCase());
-    
-    return regionMatch && searchMatch;
-  });      
-    return (
-        <>
-            <h1> Mariposas de Oceanía </h1>
-            <SearchBar 
-                onSearchChange={setSearchTerm} // Pasa la función para actualizar el término de búsqueda
-                onRegionChange={setSelectedRegion} // Pasa la función para actualizar la región
-            />
-            <div className="butterfly-list-container">
-                {filteredButterflies.map((butterfly) => ( //Accedemos a la base de datos y la recorremos mediante el map. Cada elemento que recorre se llamará butterfly (está en el paréntesis de la función)
-                    <ButterflyCard //Llamamos al componente donde se encuentran las tarjetas.
-                    key = {butterfly.id} // Identifica al elemento, escogemos id porque es único para cada mariposa. 
-                    butterfly={butterfly}
-                    />
-                )
-                )}
-            </div>
-        </>
-    ) 
-}
+    const regionMatch =
+      selectedRegion === "Todas" || butterfly.region === selectedRegion;
 
-export default List
+    // Filtro por texto de búsqueda (insensible a mayúsculas)
+    const searchMatch = butterfly.commonName
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+
+    const threatMatch = 
+        selectedThreat === "Todas" || butterfly.threatLevel === selectedThreat;
+
+    return regionMatch && searchMatch && threatMatch;
+  });
+  return (
+    <>
+      <h1> Mariposas de Oceanía </h1>
+      <SearchBar
+        onSearchChange={setSearchTerm} // Pasa la función para actualizar el término de búsqueda
+        onRegionChange={setSelectedRegion} // Pasa la función para actualizar la región
+        onThreatChange={setSelectedThreat}
+        onClearAll={handleClearFilters}
+      />
+      <div className="butterfly-list-container">
+        {filteredButterflies.map(
+          (
+            butterfly //Accedemos a la base de datos y la recorremos mediante el map. Cada elemento que recorre se llamará butterfly (está en el paréntesis de la función)
+          ) => (
+            <ButterflyCard //Llamamos al componente donde se encuentran las tarjetas.
+              key={butterfly.id} // Identifica al elemento, escogemos id porque es único para cada mariposa.
+              butterfly={butterfly}
+            />
+          )
+        )}
+      </div>
+    </>
+  );
+};
+
+export default List;
