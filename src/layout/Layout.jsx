cc74ea4bd4ed6cfa25f8666d4e6baed9e209990d
juliazmor <juliezarco21@gmail.com>
2025-07-17 12:07:04 +0200

    created butterfly card

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -9,6 +9,15 @@ const Layout = () => {
         <>
             <NavBar />
             <Outlet />
+            
+            <div className="gallery-container">
+                {
+                    butterfliesData.butterfly.map(butterfly => (
+                        <ButterflyCard key={butterfly.id} butterfly={butterfly} />
+                    ))
+                }
+            </div>
+
             <Footer />
         </>
     )
