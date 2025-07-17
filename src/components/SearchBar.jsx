f2cbb171251bc2c1cb61f92a072c7de6a1245cc8
Camila Arenas <mcarenasdh@gmail.com>
2025-07-17 12:36:39 +0200

    Commented code for better understanding

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,14 @@
-const SearchBar = ({ onSearchChange, onRegionChange }) => {
+//Vamos a crear el componente SearchBar que permitirá buscar mariposas y filtrar dependiendo las opciones que queramos.
+const SearchBar = ({ onSearchChange, onRegionChange }) => { 
+    //Recibirá por ahora dos props: 
+    //onSearchChange se usará cuando alguien escriba en la barra de búsqueda.
+   // onRegionChange se usará cuando alguien haga clic en un botón de región.
   return (
     <div>
-      <input 
+      <input //Barra de búsqueda normal
         type="text" 
         placeholder="Buscar mariposas..."
-        onChange={(e) => onSearchChange(e.target.value)} // Avisa al padre cuando se escribe
+        onChange={(e) => onSearchChange(e.target.value)} // onChange: Va detectar el cambio, se activa automáticamente (es una propiedad del input). e.target.value = recoge el valor de lo que se está escribiendo en el input.
       />
       <button onClick={() => onRegionChange('Todas')}>Todas</button>
       <button onClick={() => onRegionChange('Australia')}>Australia</button>
