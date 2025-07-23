3e027660b3c407c5c439ce3d400cb51aec4b599c
gabriela-her <ari.ghb@gmail.com>
2025-07-23 12:40:23 +0200

    try random lettern in home to prove navbar in header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <>
       <Header />
-            <h1>Especies vulnerables</h1>
+            <h1>Especies vulnerables</h1><br /><br /><h1>loshwdi</h1> <br /><br /><br /><p>sfbije</p><br /><p>dwjgwidugw</p>
       {/* {aqui deberia ir el texto que hable de las polinizadoras o asi y abajo las cards} */}
     </>
   );
