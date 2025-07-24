8fdc747b33ab48713be3b2d732140a59d42410d6
gabriela-her <ari.ghb@gmail.com>
2025-07-24 11:51:54 +0200

    feat home page with butterflycards carrusel atempt

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,45 @@
-function Home() {
-    return (
-        <>
-        {/* home */}
-        </>
-    )
-}
+import Layout from '../layout/Layout';
+import React from 'react';
+import Header from '../components/Header';  // Ajusta la ruta correcta
+import '../components/Header.css';          // Ruta CSS también correcta
+import { useState, useEffect } from 'react';
+import { getAllButterflies } from '../services/ButterflyServices';
+import ButterflyCard from '../components/ButterflyCard';
 
-export default Home
+
+const Home = () => {
+  const [butterflies, setButterflies] = useState([]);
+
+  useEffect(() => {
+    const fetchButterflyData = async () => {
+      try {
+        const bfData = await getAllButterflies();
+        const limitedData = bfData.slice(0, 10);
+        setButterflies(limitedData);
+
+
+        console.log("Mariposas: ", bfData)
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+    fetchButterflyData();
+  }, []);
+
+  return (
+
+    <>
+      <Header />
+      <div className="cards-container">
+        {butterflies.map((butterfly) => (
+          <ButterflyCard key={butterfly.id} butterfly={butterfly} />
+        ))}
+      </div>
+
+
+
+    </>
+  );
+};
+
+export default Home;
