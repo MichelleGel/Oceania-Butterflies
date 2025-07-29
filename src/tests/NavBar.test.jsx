1c6712a6a8b6040a20770b797a1d3d743546fe3f
MichelleGel <michellegelves@gmail.com>
2025-07-29 11:23:50 +0200

    Upload of test environment and files for navbar and footer test

--- src/tests/NavBar.test.jsx
+++ src/tests/NavBar.test.jsx
@@ -1,12 +1,31 @@
 import NavBar from "../components/NavBar";
 import { render, screen } from "@testing-library/react"
-import { beforeEach, expect, test } from "vitest";
+import { beforeEach, describe, expect, test } from "vitest";
+import { MemoryRouter } from "react-router-dom";
 
-beforeEach(() => {
-    render (<NavBar/>)
-})
+describe("NavBar", () => {
+    beforeEach(() => {
+        render(
+            <MemoryRouter>
+                <NavBar />
+            </MemoryRouter>
+        );
+    });
 
-test('render NavBar component with logo', () => {
-    const logoButterfly  = screen.getAllByAltText(/Logo desktop/i)
-    expect(logoButterfly).toBeDefined()
+
+
+    test('render NavBar component with logo', () => {
+        const logoButterfly = screen.getAllByAltText(/Logo desktop/i)
+        expect(logoButterfly).toBeDefined()
+    })
+
+    test('Page will go to Home when clicked on the NavBar title', () => {
+        const titleRoute = screen.getAllByText(/POLINIZADORAS | Mariposas Oceania/i)
+        expect(titleRoute).toBeDefined()
+    })
+
+    test('', () => {
+        const 
+        expect()
+    })
 })
